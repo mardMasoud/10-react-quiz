2b37c429f13898af864c0f81feec349de27a453c
masoudMarde <mardmasoud66@gmail.com>
2023-12-01 20:32:40 +0330

    version-2-4

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
 export default function Options({ question, dispatch, answer }) {
-    return (
-        <div className="options">
-            {question.options.map((option, index) => (
-                <button
-                    className={`btn btn-option ${index === answer ? "answer " : ""} ${
-                        index === question.correctOption ? "correct" : "wrong"
-                    }`}
-                    key={option}
-                    disabled={answer!==null}
-                    onClick={() => dispatch({ type: "newAnswer", payload: index })}
-                >
-                    {option}
-                </button>
-            ))}
-        </div>
-    );
+  const hasAnswerd = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? " answer" : ""} ${
+            hasAnswerd
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswerd}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
